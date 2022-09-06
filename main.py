965b5ccc92a43e3c60ced16e8362558ba4b5709c
Marcelo Araujo dos Santos <santos46ma@gmail.com>
2022-09-05 22:21:05 -0300

    Adição de função de teste de solução válida

--- main.py
+++ main.py
@@ -28,13 +28,14 @@ def resolver_M(vetor_n):
     return vetor_M
 
 
-def resolver_solucoes_particulares(vetor_a, vetor_b, vetor_n):
+def resolver_solucoes_particulares(vetor_a, vetor_b, vetor_n, vetor_M):
 
     vetor_x = []
 
     for i in range(len(vetor_a)):
-        ai, bi, ni = vetor_a[i], vetor_b[i], vetor_n[i]
-        xi = resolver_congruencia(ai, bi, ni)
+        ai, bi, ni, Mi = vetor_a[i], vetor_b[i], vetor_n[i], vetor_M[i]
+        print("passando ", [ai, bi, ni])
+        xi = resolver_congruencia(Mi, bi, ni)
         vetor_x.append(xi)
     
     return vetor_x
@@ -48,25 +49,41 @@ def achar_solucao_geral(vetor_b, vetor_M, vetor_x):
     
     return solucao
 
-
+def testar_validade_solucao(solucao, vetor_a, vetor_b, vetor_n):
+    for i in range(len(vetor_a)):
+        if (solucao*vetor_a[i]) % vetor_n[i] != vetor_b[i]:
+            return False
+    return True
 
 if __name__ == '__main__':
     # print(resolver_congruencia(35,1,3))
 
-    vetor_a = [1, 1, 1]
-    vetor_b = [1, 4, 1]
-    vetor_n = [3, 5, 7]
+    # vetor_a = [1, 1, 1]
+    # vetor_b = [1, 4, 1]
+    # vetor_n = [3, 5, 7]
 
-    vetor_x = resolver_solucoes_particulares(vetor_a, vetor_b, vetor_n)
-    print(f"solucoes {vetor_x}")
+
+    vetor_a = [2, 4, 3]
+    vetor_b = [1, 2, 4]
+    vetor_n = [3, 7, 10]
+    # sol. válida 158
+
 
     vetor_M = resolver_M(vetor_n)
 
+    # print("M ", vetor_M)
+
+    vetor_x = resolver_solucoes_particulares(vetor_a, vetor_b, vetor_n, vetor_M)
+    # print(f"solucoes {vetor_x}")
+
     solucao = achar_solucao_geral(vetor_b, vetor_M, vetor_x)
 
-    print(f"Solução: {solucao}")
+    print(f"Solução encontrada: {solucao}")
 
-
+    if testar_validade_solucao(solucao, vetor_a, vetor_b, vetor_n):
+        print("Solução Válida! 😊")
+    else:
+        print("Solução Inválida! 😔")
 
 
 
---
+++ teste.py
@@ -0,0 +1,7 @@
+from main import resolver_congruencia
+
+
+print(resolver_congruencia(35, 1, 3))
+print(resolver_congruencia(21, 1, 5))
+print(resolver_congruencia(15, 1, 7))
+
